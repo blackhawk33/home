87a236224cf09261479aeabcd4459ba39f507e65
Geoff <geoffreybowles3@gmail.com>
2019-12-31 16:14:01 +0000

    mass update

--- welcome.py
+++ welcome.py
@@ -1,1 +1,11 @@
-print("Hello world")
+#This is based on the CS50 beyond course
+
+x = int(input("Enter a value for x:"))
+
+if x > 0:
+    print("x is positive")
+elif x == 0:
+    print("x is zero")
+else:
+    print("x is negative")
+
